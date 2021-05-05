16536091523d7592eed6a0130d843074a5d41237
Swaraj Hota <swarajhota353@gmail.com>
2021-05-05 23:23:23 +0530

    fix: pblock constraint to avoid SLICEMs

--- reg.xdc
+++ reg.xdc
@@ -1,5 +1,6 @@
 create_pblock pb_slicel
+set_property IS_SOFT 0 [get_pblocks pb_slicel]
 resize_pblock pb_slicel -add {SLICE_X0Y0:SLICE_X113Y149}
 resize_pblock pb_slicel -remove [get_sites -filter {SITE_TYPE==SLICEM} -of [get_pblocks pb_slicel]]
 add_cells_to_pblock pb_slicel [get_cells reg_inst] -clear_locs
-#add_cells_to_pblock pb_slicel [get_cells reg_inst_1]
+add_cells_to_pblock pb_slicel [get_cells reg_inst_1] -clear_locs
--- top.vhd
+++ top.vhd
@@ -16,7 +16,7 @@ architecture RTL of top is
 
     attribute REG_NAME : string;
     attribute REG_NAME of reg_inst : label is "REG_16_BOOL";
-    --attribute REG_NAME of reg_inst_1 : label is "REG_32_CONST";
+    attribute REG_NAME of reg_inst_1 : label is "REG_32_CONST";
 
     -- PS7 FTMT Debug Signals
     signal ftmt_f2p_debug : std_logic_vector (31 downto 0);
@@ -29,8 +29,8 @@ architecture RTL of top is
     alias ena : std_logic is ftmt_p2f_debug(30);
     alias rst : std_logic is ftmt_p2f_debug(29);
 
-    --signal vec_tmp : std_logic_vector (31 downto 0);
-    --attribute MARK_DEBUG of vec_tmp : signal is "TRUE";
+    signal vec_tmp : std_logic_vector (31 downto 0);
+    attribute MARK_DEBUG of vec_tmp : signal is "TRUE";
 
 begin
 
@@ -50,15 +50,15 @@ begin
         in_vec => ( 0 => ftmt_p2f_debug(0), others => '0' ),
         out_vec => ftmt_f2p_debug );
 
-    --reg_inst_1 : entity work.reg
-    --generic map (
-        --IN_WIDTH => 0,
-        --OUT_WIDTH => 32 )
-    --port map (
-        --clk => clk,
-        --ena => ena,
-        --rst => rst,
-        --in_vec => ( others => '0' ),
-        --out_vec => vec_tmp );
+    reg_inst_1 : entity work.reg
+    generic map (
+        IN_WIDTH => 0,
+        OUT_WIDTH => 32 )
+    port map (
+        clk => clk,
+        ena => ena,
+        rst => rst,
+        in_vec => ( others => '0' ),
+        out_vec => vec_tmp );
 
 end architecture RTL;
